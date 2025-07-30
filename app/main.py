1fd66d7134923e93738174dae63e214ebe84feb5
Matvke <matvei31055@gmail.com>
2025-07-31 01:53:47 +0500

    completely redone

--- app/main.py
+++ app/main.py
@@ -1,72 +1,73 @@
 import os
 from pathlib import Path
 
+CONNECTOR_BRANCH = "├── "
+CONNECTOR_LAST_BRANCH = "└── "
+PREFIX_PIPE = "│   "
+PREFIX_EMPTY = "    "
 
 class TreeGenerator():
     def __init__(self, 
+                 root_dir: str, 
                  ignore_list: list = [], 
                  read_gitignore: bool = True, 
-                 gitignore_path: str = '.gitignore', 
-                 readme_path: str = 'README.md'):
+                 gitignore_path: str = '.gitignore'):
         self.ignore_list = ignore_list
-        self.read_gitignore = read_gitignore
-        self.readme_path = readme_path
-        
+        self.root_dir = root_dir
+
+        if read_gitignore:
+            self.ignore_list.extend(self._read_gitignore(gitignore_path))
+
+    def _read_gitignore(self, gitignore_path) -> list:
+        gitignore_list = []
         with open(gitignore_path) as file:
-            if read_gitignore:
-                for line in file.readlines():
-                    if line[0] == '#':
-                        continue
-                    self.ignore_list.append(line.strip().replace('/', ''))
+            for line in file.readlines():
+                if line[0] == '#':
+                    continue
+                gitignore_list.append(line.strip().replace('/', ''))
+        return gitignore_list
+
+    def _build_tree(self, curr_path):
+        tree = {}
+
+        for entity in sorted(os.scandir(curr_path), key=lambda e: e.name):
+            path_entity = Path(entity)
+            if self._is_ignored(path_entity):
+                continue
+
+            print(path_entity)
+            if entity.is_dir():
+                tree[entity.name] = self._build_tree(path_entity)
+            elif entity.is_file():
+                tree[entity.name] = None
+        return tree
+    
+    def _format_tree(self, tree_dict: dict, prefix: str = '') -> list[str]:
+        lines = []
+        entities = list(tree_dict.keys())
+
+        for i, name in enumerate(entities):
+            is_last = (i == len(entities) - 1)
+            connector = CONNECTOR_LAST_BRANCH if is_last else CONNECTOR_BRANCH
+            lines.append(f"{prefix}{connector}{name}")
+
+            if isinstance(tree_dict[name], dict):
+                parent_prefix = PREFIX_EMPTY if is_last else PREFIX_PIPE
+                lines.extend(self._format_tree(tree_dict[name], prefix=prefix + parent_prefix))
+
+        return lines
+
+    def generate(self) -> str:
+        tree_structure = self._build_tree(self.root_dir)
+        tree_lines = self._format_tree(tree_structure)
+        return f"{self.root_dir}{os.sep} \n" + "\n".join(tree_lines)
 
 
-    def draw_tree(self, path: str):
-        def ignore_condition(dir: str):
-            return dir not in self.ignore_list and not dir.startswith(('.', '_')) 
-        
-        with open(self.readme_path, 'at', encoding="UTF-8") as file:
-            file.write("# Directory tree\n\n```")
-            for dirpath, dirnames, filenames in os.walk(path):
-                dirnames[:] = [d for d in dirnames if ignore_condition(d)]
-                filenames[:] = [f for f in filenames if not f.startswith(('_'))]
-                depth = len(Path(os.path.relpath(dirpath, Path(path))).parents)
-                rel_path = os.path.relpath(dirpath, path)
-                has_siblings = False
-
-                if rel_path != '.':
-                    parent_path = "\\".join(dirpath.split('\\')[0:-1])
-                    _, sibling_dirs, _ = next(os.walk(parent_path))
-                    sibling_dirs = [d for d in os.listdir(parent_path) 
-                                    if ignore_condition(d) 
-                                    and os.path.isdir(os.path.join(parent_path, d))]
-                    
-                    sibling_dirs.remove(Path(dirpath).name)
-                    has_siblings = len(sibling_dirs) > 0
-                
-                indent = '│   ' * (depth)
-                need_indent = True
-                if depth == 0:
-                    dir_start_symbol = "" 
-                elif not has_siblings:
-                    dir_start_symbol = "└── "
-                else:
-                    dir_start_symbol = "├── "
-
-                if len(dirnames) == 0 and not has_siblings:
-                    need_indent = False
-
-                file.write(f"{indent}{dir_start_symbol}{os.path.basename(dirpath)}/\n")
-                for j, filename in enumerate(filenames):
-                    file_start_symbol = "└── " if j == len(filenames) - 1 and len(dirnames) == 0 else "├── "
-                    indent = '│   ' * (depth + 1) if need_indent else f"{'│   ' * (depth)}    "  
-                    file.write(f"{indent}{file_start_symbol}{filename}\n")
-
-                self.ignore_list.append(Path(dirpath).name)
-            file.write('```')
-
+    def _is_ignored(self, path: Path):
+        return path.name in self.ignore_list or path.name.startswith(('.', '_')) 
 
 
 # # curr_dir = input("Input curr directory path:")
 curr_dir = r"C:\Users\matve\api_creater"
-tree_generator = TreeGenerator(readme_path='README.md')
-tree_generator.draw_tree(curr_dir)
+tree_generator = TreeGenerator(curr_dir)
+print(tree_generator.generate())
